bcd93765426f89cb12e752371cace37328c05fb1
Emanuele-Ficerai <emanuele.ficerai@gmail.com>
2024-04-22 13:31:30 +0200

    edit

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -28,4 +28,54 @@ print("Any of these animals would make a great pet",l)
 number: list[21]
 for number in range(21):
     print(number)
-    
+
+#-4. One Million: Make a list of the numbers from one to one million, and then use a for loop to print the numbers.
+#(If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
+numbers: list=[]
+for i in range(1, 1000001):
+        #print(i)
+        numbers.append(i)
+
+#    4-5. Summing a Million: Make a list of the numbers from one to one million, 
+#and then use min() and max() to make sure your list actually starts at one and ends at one million. 
+#Also, use the sum() function to see how quickly Python can add a million numbers.  
+numbers: list=[]
+for i in range(1, 1000001):
+        #print(i)
+        numbers.append(i)
+x = min(numbers)
+y = max(numbers)
+z = sum(numbers) 
+print(z)
+
+#4-6. Odd Numbers: Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. 
+#Use a for loop to print each number.
+for i in range(1, 20, 2):   
+      print(i)
+
+#4-7. Threes: Make a list of the multiples of 3, from 3 to 30. Use a for loop to print the numbers in your list.
+for i in range(3, 33, 3):
+      print(i)
+
+#4-8. Cubes: A number raised to the third power is called a cube. For example, the cube of 2 is written as 2**3 in Python. 
+#Make a list of the first 10 cubes (that is, the cube of each integer from 1 through 10), and use a for loop to print out the value of each cube.
+for power in range(1, 10):
+      print(power**3)
+
+#4-9. Cube Comprehension: Use a list comprehension to generate a list of the first 10 cubes.
+
+#4-10. Slices: Using one of the programs you wrote in this chapter, add several lines to the end of the program that do the following:
+#• Print the message The first three items in the list are:. Then use a slice to print the first three items from that program’s list.
+#• Print the message Three items from the middle of the list are:. Then use a slice to print three items from the middle of the list.
+#• Print the message The last three items in the list are:. Then use a slice to print the last three items in the list.
+Kinds: list=["Margherita", "Diavola", "Boscaiola", "Fiori", "Bianca"]
+for favorite in Kinds:
+    print(favorite)
+    print(f"my favorite kinds of pizza {favorite}")
+print("i like pizza")
+print(f"{Kinds[:3]} the first three items in the list are") #:3= per i primi tre argomenti all'interno della lista
+print(f"{Kinds[1:4]} three items from the middle of the list are")#: = con i 2 punti stiamo prndendo i valori compresi  da 1 (per esempio) a 4(perchè lultima posizione come nel range non la prende)
+print(f"{Kinds[-3:]} the last three items in the list are")# -3= per gli ultimi tre argomenti della lista
+
+
+ 
